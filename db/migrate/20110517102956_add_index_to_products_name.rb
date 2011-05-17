6f70d9d7689ee7206fbd849115002c5cf1491f64
David B <thirdreplicator@gmail.com>
2011-05-17 18:46:47 +0800

    ordering products by name.

---
+++ db/migrate/20110517102956_add_index_to_products_name.rb
@@ -0,0 +1,9 @@
+class AddIndexToProductsName < ActiveRecord::Migration
+  def self.up
+    add_index :products, :name
+  end
+
+  def self.down
+    remove_index :products, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110517090545) do
+ActiveRecord::Schema.define(:version => 20110517102956) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20110517090545) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "products", ["name"], :name => "index_products_on_name"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
