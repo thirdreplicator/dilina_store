e9f9df62ecefbf903efddbe556e0a53717a76f3f
David Beckwith <thirdreplicator@gmail.com>
2011-05-19 01:20:46 -0700

    Load products database and index it in Javascript.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @products }
+      format.json { render :json => @products }
     end
   end
 
